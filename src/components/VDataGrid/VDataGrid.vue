0b2453a1b14f3159d6961dce36c8209efff54fe2
Paolo Forti <fortipaolo96@gmail.com>
2023-11-20 20:27:10 +0100

    fixed wrong input management issues

--- src/components/VDataGrid/VDataGrid.vue
+++ src/components/VDataGrid/VDataGrid.vue
@@ -1,73 +1,74 @@
 <template>
-	<div
-		class="flex flex-col p-2 gap-2 bg-color-bg min-h-0 min-w-0 h-full w-full rounded-lg overflow-hidden border border-color-border-100"
-	>
-		<!--GRID LEADING CONTENT-->
-		<div v-if="$slots.header">
-			<slot name="header" />
-		</div>
-		<div
-			class="relative min-h-0 flex flex-col h-full border border-color-border-200"
-			ref="tableContainer"
-		>
-			<!-- GRID CONTENT -->
-			<VirtualScroller
-				:item-height="32"
-				:items="data"
-				:wrapper-class="initialized ? 'w-fit' : ''"
-				ref="content"
-			>
-				<template #prepend>
-					<!-- GRID HEADER -->
-					<div
-						class="sticky grid top-0 bg-color-bg-50 z-10 border-b border-color-border-200"
-						:class="{ 'w-fit': initialized }"
-						:style="{
-							gridTemplateColumns: columnsLayout || columnsGridLayout,
-						}"
-						ref="columnsContainer"
-					>
-						<VDataGridColumn
-							v-for="(column, index) in columns"
-							:data="column"
-							:index="index"
-							:formatter="columnFormatter || textFormatter"
-							:translator="translator || i18n"
-							class="px-3 leading-[32px]"
-						/>
-					</div>
-				</template>
-				<!-- GRID ROWS -->
-				<template v-slot="{ item, index, height }">
-					<VDataGridRow
-						:data="item"
-						:index="index"
-						:cellHeight="`${height - 1}px`"
-						:style="[
-							{
-								height: `${height}px`,
-								gridTemplateColumns: columnsLayout || columnsGridLayout,
-							},
-						]"
-						@dblclick="emit('rowDoubleClick', item)"
-					/>
-				</template>
-			</VirtualScroller>
-			<!--GRID TRAILING CONTENT-->
-		</div>
-	</div>
+  <div
+    class="flex flex-col p-2 gap-2 bg-color-bg min-h-0 min-w-0 h-full w-full rounded-lg overflow-hidden border border-color-border-100"
+  >
+    <!--GRID LEADING CONTENT-->
+    <div v-if="$slots.header">
+      <slot name="header" />
+    </div>
+    <div
+      class="relative min-h-0 flex flex-col h-full border border-color-border-200"
+      ref="tableContainer"
+      tabindex="-1"
+    >
+      <!-- GRID CONTENT -->
+      <VirtualScroller
+        :item-height="32"
+        :items="data"
+        :wrapper-class="initialized ? 'w-fit' : ''"
+        ref="content"
+      >
+        <template #prepend>
+          <!-- GRID HEADER -->
+          <div
+            class="sticky grid top-0 bg-color-bg-50 z-10 border-b border-color-border-200"
+            :class="{ 'w-fit': initialized }"
+            :style="{
+              gridTemplateColumns: columnsLayout || columnsGridLayout,
+            }"
+            ref="columnsContainer"
+          >
+            <VDataGridColumn
+              v-for="(column, index) in columns"
+              :data="column"
+              :index="index"
+              :formatter="columnFormatter || textFormatter"
+              :translator="translator || i18n"
+              class="px-3 leading-[32px]"
+            />
+          </div>
+        </template>
+        <!-- GRID ROWS -->
+        <template v-slot="{ item, index, height }">
+          <VDataGridRow
+            :data="item"
+            :index="index"
+            :cellHeight="`${height - 1}px`"
+            :style="[
+              {
+                height: `${height}px`,
+                gridTemplateColumns: columnsLayout || columnsGridLayout,
+              },
+            ]"
+            @dblclick="emit('rowDoubleClick', item)"
+          />
+        </template>
+      </VirtualScroller>
+      <!--GRID TRAILING CONTENT-->
+    </div>
+  </div>
 </template>
 
 <script lang="ts" setup>
 import VirtualScroller from "@/components/VirtualScroller/VirtualScroller.vue";
 import {
-	ExplicitTranslationValue,
-	KeyTranslationValue,
-	RawTranslationValue,
-	TranslationType,
-	VDataColumn,
-	VDataGridEmits,
-	VDataRow,
+  ExplicitTranslationValue,
+  KeyTranslationValue,
+  RawTranslationValue,
+  TranslationType,
+  VDataColumn,
+  VDataGridEmits,
+  VDataRow,
 } from "@/enums";
 import { textFormatter } from "@/formatters/index";
 import { computed, onMounted, provide, ref, watchEffect } from "vue";
@@ -79,46 +80,46 @@ import VDataGridState from "./VDataGridState";
 const i18n = useI18n();
 
 const props = withDefaults(
-	defineProps<{
-		columns: Array<VDataColumn>;
-		rows: Array<VDataRow>;
-		defaultSortKey?: string;
-		defaultSortDirection?: string;
-		columnFormatter?: (
-			value: {
-				type: TranslationType;
-				value:
-					| ExplicitTranslationValue
-					| KeyTranslationValue
-					| RawTranslationValue;
-			},
-			translator: any
-		) => string;
-		translator?: any;
-	}>(),
-	{}
+  defineProps<{
+    columns: Array<VDataColumn>;
+    rows: Array<VDataRow>;
+    defaultSortKey?: string;
+    defaultSortDirection?: string;
+    columnFormatter?: (
+      value: {
+        type: TranslationType;
+        value:
+          | ExplicitTranslationValue
+          | KeyTranslationValue
+          | RawTranslationValue;
+      },
+      translator: any
+    ) => string;
+    translator?: any;
+  }>(),
+  {}
 );
 
 const emit = defineEmits<VDataGridEmits>();
 
 const columnsGridLayout = computed(() =>
-	props.columns ? `repeat(${props.columns.length}, 1fr)` : ""
+  props.columns ? `repeat(${props.columns.length}, 1fr)` : ""
 );
 
 const state = VDataGridState(
-	props.rows,
-	props.columns,
-	props.defaultSortKey,
-	props.defaultSortDirection,
-	emit
+  props.rows,
+  props.columns,
+  props.defaultSortKey,
+  props.defaultSortDirection,
+  emit
 );
 
 watchEffect(() => {
-	state.updateRows(props.rows);
+  state.updateRows(props.rows);
 });
 
 const { initialized, columnsLayout, data, resetSelection, selectedRowId } =
-	state;
+  state;
 
 const tableContainer = ref<HTMLElement | null>(null);
 const columnsContainer = ref<HTMLElement | null>(null);
@@ -127,27 +128,28 @@ const content = ref<InstanceType<typeof VirtualScroller> | null>(null);
 provide("state", state);
 
 defineExpose({
-	rows: data,
-	scrollTo: (value: number) => {
-		content.value?.scrollToIndex(value);
-	},
-	resetSelection,
-	selectedRowId,
+  rows: data,
+  rowsWrapper: content.value?.itemsWrapper,
+  scrollTo: (value: number) => {
+    content.value?.scrollToIndex(value);
+  },
+  resetSelection,
+  selectedRowId,
 });
 
 onMounted(() => {
-	requestAnimationFrame(() => {
-		if (
-			tableContainer.value !== null &&
-			columnsContainer.value !== null &&
-			content.value !== null
-		) {
-			state.init(
-				tableContainer.value!,
-				columnsContainer.value!,
-				content.value!.container!
-			);
-		}
-	});
+  requestAnimationFrame(() => {
+    if (
+      tableContainer.value !== null &&
+      columnsContainer.value !== null &&
+      content.value !== null
+    ) {
+      state.init(
+        tableContainer.value!,
+        columnsContainer.value!,
+        content.value!.container!
+      );
+    }
+  });
 });
 </script>
--- src/components/VDataGrid/VDataGridState.ts
+++ src/components/VDataGrid/VDataGridState.ts
@@ -1,358 +1,377 @@
 import { useSorter } from "@/composables/UseSorter";
 import {
-	CalculatedElementSize,
-	VDataColumn,
-	VDataGridEmits,
-	VDataRow,
+  CalculatedElementSize,
+  VDataColumn,
+  VDataGridEmits,
+  VDataRow,
 } from "@/enums";
 import {
-	clamp,
-	onKeyStroke,
-	onStartTyping,
-	useElementSize,
+  clamp,
+  onKeyStroke,
+  onStartTyping,
+  useElementSize,
 } from "@vueuse/core";
 import { computed, ref } from "vue";
 import { useFilter } from "../../composables/UseFilter";
 
 type DataGridColumnData = {
-	id: string;
-	el: HTMLElement;
-	size: CalculatedElementSize;
-	offset?: number;
+  id: string;
+  el: HTMLElement;
+  size: CalculatedElementSize;
+  offset?: number;
 };
 
 function VDataGridState(
-	rows: VDataRow[],
-	columns: VDataColumn[],
-	defaultSortKey: string | undefined,
-	defaultSortDirection: string | undefined,
-	emit: VDataGridEmits
+  rows: VDataRow[],
+  columns: VDataColumn[],
+  defaultSortKey: string | undefined,
+  defaultSortDirection: string | undefined,
+  emit: VDataGridEmits
 ) {
-	const tableContainerRef = ref<HTMLElement>();
-	const columnsContainerRef = ref<HTMLElement>();
-	const contentContainerRef = ref<HTMLElement>();
-	const columnsDataList = ref<DataGridColumnData[]>([]);
-	const lockedColumnsMap = ref<Map<string, number>>(new Map<string, number>());
-	const initialized = ref(false);
+  const tableContainerRef = ref<HTMLElement>();
+  const columnsContainerRef = ref<HTMLElement>();
+  const contentContainerRef = ref<HTMLElement>();
+  const columnsDataList = ref<DataGridColumnData[]>([]);
+  const lockedColumnsMap = ref<Map<string, number>>(new Map<string, number>());
+  const initialized = ref(false);
 
-	const selectedRowId = ref<number>(0);
-	const selectedColumnId = ref<string>("");
-	const selectedCellId = ref<string>("");
+  const selectedRowId = ref<number>(0);
+  const selectedColumnId = ref<string>("");
+  const selectedCellId = ref<string>("");
 
-	const activeCell = ref<HTMLElement>();
+  const activeCell = ref<HTMLElement>();
 
-	const editMode = ref(false);
+  const editMode = ref(false);
 
-	const reactiveRows = ref(rows);
+  const reactiveRows = ref(rows);
 
-	const { width, height } = useElementSize(contentContainerRef);
-	const { data, filters, setFilter, resetFilter } =
-		useFilter<VDataRow>(reactiveRows);
+  const { width, height } = useElementSize(contentContainerRef);
+  const { data, filters, setFilter, resetFilter } =
+    useFilter<VDataRow>(reactiveRows);
 
-	function updateRows(updatedRows: VDataRow[]) {
-		reactiveRows.value = updatedRows;
-	}
+  function updateRows(updatedRows: VDataRow[]) {
+    reactiveRows.value = updatedRows;
+  }
 
-	const { sort, sortKey, sortOrder, applySort } = useSorter<VDataRow>(
-		defaultSortKey,
-		defaultSortDirection
-	);
+  const { sort, sortKey, sortOrder, applySort } = useSorter<VDataRow>(
+    defaultSortKey,
+    defaultSortDirection
+  );
 
-	const columnsLayout = computed(() =>
-		columnsDataList.value
-			.map((column: DataGridColumnData) =>
-				column.size.current === -1
-					? `minmax(${column.size.min}px, 1fr)`
-					: `${column.size.current!}px`
-			)
-			.join(" ")
-	);
+  const columnsLayout = computed(() =>
+    columnsDataList.value
+      .map((column: DataGridColumnData) =>
+        column.size.current === -1
+          ? `minmax(${column.size.min}px, 1fr)`
+          : `${column.size.current!}px`
+      )
+      .join(" ")
+  );
 
-	function init(
-		tableContainer: HTMLElement,
-		columnsContainer: HTMLElement,
-		contentContainer: HTMLElement
-	) {
-		tableContainerRef.value = tableContainer;
-		columnsContainerRef.value = columnsContainer;
-		contentContainerRef.value = contentContainer;
+  function init(
+    tableContainer: HTMLElement,
+    columnsContainer: HTMLElement,
+    contentContainer: HTMLElement
+  ) {
+    tableContainerRef.value = tableContainer;
+    columnsContainerRef.value = columnsContainer;
+    contentContainerRef.value = contentContainer;
 
-		requestAnimationFrame(() => {
-			initializeColumnsData();
-			refreshColumnsData();
-			initialized.value = true;
-		});
-	}
+    requestAnimationFrame(() => {
+      initializeColumnsData();
+      refreshColumnsData();
+      initialized.value = true;
+    });
+  }
 
-	function initializeColumnsData() {
-		if (!columnsContainerRef.value) return;
-		Array.from(columnsContainerRef.value.children).forEach(
-			(column: Element, index: number) => {
-				const columnProps: VDataColumn = columns[index];
+  function initializeColumnsData() {
+    if (!columnsContainerRef.value) return;
+    Array.from(columnsContainerRef.value.children).forEach(
+      (column: Element, index: number) => {
+        const columnProps: VDataColumn = columns[index];
 
-				const columnData: DataGridColumnData = {
-					id: columnProps.id,
-					el: column as HTMLElement,
-					size: {
-						current:
-							columnProps.size && columnProps.size.initial
-								? columnProps.size.initial
-								: -1,
-						max:
-							columnProps.size && columnProps.size.max
-								? columnProps.size.max
-								: width.value,
-						min:
-							columnProps.size && columnProps.size.min
-								? columnProps.size.min
-								: 30,
-					},
-				};
+        const columnData: DataGridColumnData = {
+          id: columnProps.id,
+          el: column as HTMLElement,
+          size: {
+            current:
+              columnProps.size && columnProps.size.initial
+                ? columnProps.size.initial
+                : -1,
+            max:
+              columnProps.size && columnProps.size.max
+                ? columnProps.size.max
+                : width.value,
+            min:
+              columnProps.size && columnProps.size.min
+                ? columnProps.size.min
+                : 30,
+          },
+        };
 
-				if (columnData.size.current! !== -1) {
-					columnData.size.current = clamp(
-						columnData.size.current!,
-						columnData.size.min!,
-						columnData.size.max!
-					);
-				}
+        if (columnData.size.current! !== -1) {
+          columnData.size.current = clamp(
+            columnData.size.current!,
+            columnData.size.min!,
+            columnData.size.max!
+          );
+        }
 
-				if (columnProps.locked) {
-					lockedColumnsMap.value.set(columnProps.id, 0);
-					columnData.offset = 0;
-				}
+        if (columnProps.locked) {
+          lockedColumnsMap.value.set(columnProps.id, 0);
+          columnData.offset = 0;
+        }
 
-				columnsDataList.value.push(columnData);
-			}
-		);
-	}
+        columnsDataList.value.push(columnData);
+      }
+    );
+  }
 
-	function refreshColumnsData() {
-		columnsDataList.value.forEach(
-			(columnData: DataGridColumnData, index: number) => {
-				if (columnData.size.current! === -1) {
-					columnData.size.current = columnData.el.getBoundingClientRect().width;
-				}
+  function refreshColumnsData() {
+    columnsDataList.value.forEach(
+      (columnData: DataGridColumnData, index: number) => {
+        if (columnData.size.current! === -1) {
+          columnData.size.current = columnData.el.getBoundingClientRect().width;
+        }
 
-				columnData.size.current = clamp(
-					columnData.size.current!,
-					columnData.size.min!,
-					columnData.size.max!
-				);
+        columnData.size.current = clamp(
+          columnData.size.current!,
+          columnData.size.min!,
+          columnData.size.max!
+        );
 
-				if (index > 0 && columnData.offset !== undefined) {
-					columnData.offset =
-						columnsDataList.value[index - 1].offset! +
-						columnsDataList.value[index - 1].size.current!;
-					lockedColumnsMap.value.set(columnData.id, columnData.offset);
-				}
-			}
-		);
-	}
+        if (index > 0 && columnData.offset !== undefined) {
+          columnData.offset =
+            columnsDataList.value[index - 1].offset! +
+            columnsDataList.value[index - 1].size.current!;
+          lockedColumnsMap.value.set(columnData.id, columnData.offset);
+        }
+      }
+    );
+  }
 
-	function updateColumnSize(columnId: string, width: number) {
-		const columnDataIndex: number = columnsDataList.value.findIndex(
-			(column: DataGridColumnData) => column.id === columnId
-		);
+  function updateColumnSize(columnId: string, width: number) {
+    const columnDataIndex: number = columnsDataList.value.findIndex(
+      (column: DataGridColumnData) => column.id === columnId
+    );
 
-		if (columnDataIndex !== -1) {
-			const columnData: DataGridColumnData =
-				columnsDataList.value[columnDataIndex];
+    if (columnDataIndex !== -1) {
+      const columnData: DataGridColumnData =
+        columnsDataList.value[columnDataIndex];
 
-			columnData.size.current = clamp(
-				width,
-				columnData.size.min!,
-				columnData.size.max!
-			);
+      columnData.size.current = clamp(
+        width,
+        columnData.size.min!,
+        columnData.size.max!
+      );
 
-			const nextColumnData: DataGridColumnData =
-				columnsDataList.value[columnDataIndex + 1];
-			if (nextColumnData && nextColumnData.offset) {
-				nextColumnData.offset = columnData.offset! + columnData.size.current!;
-				lockedColumnsMap.value.set(nextColumnData.id, nextColumnData.offset);
-			}
-		}
-	}
+      const nextColumnData: DataGridColumnData =
+        columnsDataList.value[columnDataIndex + 1];
+      if (nextColumnData && nextColumnData.offset) {
+        nextColumnData.offset = columnData.offset! + columnData.size.current!;
+        lockedColumnsMap.value.set(nextColumnData.id, nextColumnData.offset);
+      }
+    }
+  }
 
-	function changeSelectedCell(rowId: number, columnId: string, cellId: string) {
-		if (editMode.value) {
-			editMode.value = false;
-			onCellEditEnd();
-		}
+  function changeSelectedCell(rowId: number, columnId: string, cellId: string) {
+    if (editMode.value) {
+      editMode.value = false;
+      onCellEditEnd();
+    }
 
-		selectedRowId.value = rowId;
-		selectedColumnId.value = columnId;
-		selectedCellId.value = cellId;
+    selectedRowId.value = rowId;
+    selectedColumnId.value = columnId;
+    selectedCellId.value = cellId;
 
-		const match = tableContainerRef.value?.querySelector(`.cell-${cellId}`);
-		if (match) {
-			(match as HTMLElement).focus();
-			activeCell.value = match as HTMLElement;
-		}
+    const match = tableContainerRef.value?.querySelector(`.cell-${cellId}`);
+    if (match) {
+      (match as HTMLElement).focus();
+      activeCell.value = match as HTMLElement;
+    }
 
-		emit("rowClick", getSelectedRow());
-	}
+    emit("rowClick", getSelectedRow());
+  }
 
-	onKeyStroke(
-		[
-			"ArrowDown",
-			"ArrowUp",
-			"ArrowLeft",
-			"ArrowRight",
-			"Escape",
-			"Tab",
-			"Enter",
-			" ",
-		],
-		(e) => {
-			if (e.key === " ") {
-				e.preventDefault();
-				return;
-			}
+  onKeyStroke(
+    [
+      "ArrowDown",
+      "ArrowUp",
+      "ArrowLeft",
+      "ArrowRight",
+      "Escape",
+      "Tab",
+      "Enter",
+      " ",
+    ],
+    (e) => {
+      if (!cellFocused() && !editMode.value) return;
 
-			let direction = {
-				x: 0,
-				y: 0,
-			};
+      if (e.key === " ") {
+        return;
+      }
 
-			if (editMode.value) {
-				if (e.key === "Tab" || e.key === "Enter") {
-					e.preventDefault();
-					direction.y = 0;
-					if (e.shiftKey) {
-						direction.x = -1;
-					} else {
-						direction.x = 1;
-					}
-					onCellEditEnd();
-				} else if (e.key === "Escape") {
-					editMode.value = false;
-				} else {
-					return;
-				}
-			} else {
-				e.preventDefault();
-				direction = {
-					x: +(e.key === "ArrowRight") - +(e.key === "ArrowLeft"),
-					y: +(e.key === "ArrowDown") - +(e.key === "ArrowUp"),
-				};
-			}
+      let direction = {
+        x: 0,
+        y: 0,
+      };
 
-			if (getColumnIndex() + direction.x < 0 && getRowIndex() > 0) {
-				direction.x = 99999;
-				direction.y = -1;
-			} else if (
-				getColumnIndex() + direction.x >= columns.length &&
-				getRowIndex() < data.value.length - 1
-			) {
-				direction.x = -99999;
-				direction.y = 1;
-			}
+      if (editMode.value) {
+        if (e.key === "Tab" || e.key === "Enter") {
+          e.preventDefault();
+          direction.y = 0;
+          if (e.shiftKey) {
+            direction.x = -1;
+          } else {
+            direction.x = 1;
+          }
+          onCellEditEnd();
+        } else if (e.key === "Escape") {
+          editMode.value = false;
+        } else {
+          return;
+        }
+      } else {
+        e.preventDefault();
+        direction = {
+          x: +(e.key === "ArrowRight") - +(e.key === "ArrowLeft"),
+          y: +(e.key === "ArrowDown") - +(e.key === "ArrowUp"),
+        };
+      }
 
-			const nextRowIndex = clamp(
-				getRowIndex() + direction.y,
-				0,
-				data.value.length - 1
-			);
-			const nextColumnIndex = clamp(
-				getColumnIndex() + direction.x,
-				0,
-				columns.length - 1
-			);
+      if (getColumnIndex() + direction.x < 0 && getRowIndex() > 0) {
+        direction.x = 99999;
+        direction.y = -1;
+      } else if (
+        getColumnIndex() + direction.x >= columns.length &&
+        getRowIndex() < data.value.length - 1
+      ) {
+        direction.x = -99999;
+        direction.y = 1;
+      }
 
-			const rowId = data.value[nextRowIndex].id;
-			const columnId = columns[nextColumnIndex].id;
-			const cellId = `${rowId}-${columnId}`;
-			changeSelectedCell(rowId, columnId, `${cellId}`);
-		},
-		{ target: tableContainerRef.value }
-	);
+      const nextRowIndex = clamp(
+        getRowIndex() + direction.y,
+        0,
+        data.value.length - 1
+      );
+      const nextColumnIndex = clamp(
+        getColumnIndex() + direction.x,
+        0,
+        columns.length - 1
+      );
 
-	onStartTyping(() => {
-		const column = columns[getColumnIndex()];
-		if (activeCell.value && column?.editable) {
-			editMode.value = true;
-			if (column.dataType === undefined) {
-				getSelectedRow()![selectedColumnId.value] = "";
-			}
-		}
-	});
+      const rowId = data.value[nextRowIndex].id;
+      const columnId = columns[nextColumnIndex].id;
+      const cellId = `${rowId}-${columnId}`;
+      changeSelectedCell(rowId, columnId, `${cellId}`);
+    },
+    { target: tableContainerRef.value }
+  );
 
-	function getSelectedRow() {
-		return data.value.find((row: VDataRow) => row.id === selectedRowId.value);
-	}
+  onStartTyping(() => {
+    if (
+      getSelectedCell() === null ||
+      getSelectedCell() !== document.activeElement
+    )
+      return;
 
-	function getRowIndex() {
-		return data.value.findIndex(
-			(row: VDataRow) => row.id === selectedRowId.value
-		);
-	}
+    const column = columns[getColumnIndex()];
+    if (activeCell.value && column?.editable) {
+      editMode.value = true;
+      if (column.dataType === undefined) {
+        getSelectedRow()![selectedColumnId.value] = "";
+      }
+    }
+  });
 
-	function getColumnIndex() {
-		return columns.findIndex(
-			(column: VDataColumn) => column.id === selectedColumnId.value
-		);
-	}
+  function cellFocused() {
+    return (
+      getSelectedCell() !== null && getSelectedCell() === document.activeElement
+    );
+  }
 
-	function onCellEditEnd() {
-		const column: VDataColumn = columns[getColumnIndex()];
+  function getSelectedCell() {
+    return tableContainerRef.value?.querySelector(
+      `.cell-${selectedRowId.value}-${selectedColumnId.value}`
+    );
+  }
 
-		if (column && column.editable) {
-			const row: VDataRow | undefined = rows.find(
-				(row: VDataRow) => row.id === selectedRowId.value
-			);
-			if (row) {
-				// console.log("NEW VALUE", row[column.id]);
+  function getSelectedRow() {
+    return data.value.find((row: VDataRow) => row.id === selectedRowId.value);
+  }
 
-				emit("cellValueChanged", {
-					row,
-					column,
-					newValue: row[column.id],
-				});
-			}
-		}
-	}
+  function getRowIndex() {
+    return data.value.findIndex(
+      (row: VDataRow) => row.id === selectedRowId.value
+    );
+  }
 
-	function resetSelection() {
-		changeSelectedCell(-1, "", "");
-	}
+  function getColumnIndex() {
+    return columns.findIndex(
+      (column: VDataColumn) => column.id === selectedColumnId.value
+    );
+  }
 
-	return {
-		// methods
-		init,
-		updateColumnSize,
-		updateRows,
-		resetSelection,
+  function onCellEditEnd() {
+    const column: VDataColumn = columns[getColumnIndex()];
 
-		changeSelectedCell,
+    if (column && column.editable) {
+      const row: VDataRow | undefined = rows.find(
+        (row: VDataRow) => row.id === selectedRowId.value
+      );
+      if (row) {
+        // console.log("NEW VALUE", row[column.id]);
 
-		sort,
+        emit("cellValueChanged", {
+          row,
+          column,
+          newValue: row[column.id],
+        });
+      }
+    }
+  }
 
-		filters,
-		setFilter,
-		resetFilter,
+  function resetSelection() {
+    changeSelectedCell(-1, "", "");
+  }
 
-		onCellEditEnd,
+  return {
+    // methods
+    init,
+    updateColumnSize,
+    updateRows,
+    resetSelection,
 
-		// computed properties
-		columnsLayout,
+    changeSelectedCell,
 
-		data: computed(() => applySort(data)),
-		sortKey,
-		sortOrder,
+    sort,
 
-		initialized,
-		tableHeight: height,
-		lockedColumnsMap,
+    filters,
+    setFilter,
+    resetFilter,
 
-		columns,
+    onCellEditEnd,
 
-		selectedRowId,
-		selectedColumnId,
-		selectedCellId,
+    // computed properties
+    columnsLayout,
 
-		editMode,
-	};
+    data: computed(() => applySort(data)),
+    sortKey,
+    sortOrder,
+
+    initialized,
+    tableHeight: height,
+    lockedColumnsMap,
+
+    columns,
+
+    selectedRowId,
+    selectedColumnId,
+    selectedCellId,
+
+    editMode,
+  };
 }
 
 export default VDataGridState;
