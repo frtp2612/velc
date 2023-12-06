e5e8fb733a7eac32333773f9afe2f4c0c9f6bdaa
Paolo Forti <fortipaolo96@gmail.com>
2023-12-06 21:47:16 +0100

    data grid fixes

--- src/components/VCheckBox/VCheckBox.vue
+++ src/components/VCheckBox/VCheckBox.vue
@@ -1,91 +1,85 @@
 <template>
-	<div
-		class="cursor-pointer flex items-center justify-center gap-2"
-		@click="toggleValue"
-	>
-		<div
-			class="input-base !p-0 !h-[1.25rem] !w-[1.25rem] flex items-center justify-center"
-			:class="[checkboxClass]"
-			tabindex="0"
-		>
-			<font-awesome-icon
-				:icon="checkboxIcon"
-				class="h-2.5 w-2.5"
-				v-if="checkboxIcon"
-			/>
-		</div>
-		<VLabel class="font-medium" v-if="label">{{ label }}</VLabel>
-	</div>
+  <div
+    class="cursor-pointer flex items-center justify-center gap-2"
+    @click="toggleValue"
+  >
+    <div
+      class="input-base !p-0 !h-[1.25rem] !w-[1.25rem] flex items-center justify-center"
+      :class="[checkboxClass]"
+      tabindex="0"
+    >
+      <font-awesome-icon
+        :icon="checkboxIcon"
+        class="h-2.5 w-2.5"
+        v-if="checkboxIcon"
+      />
+    </div>
+    <VLabel class="font-medium" v-if="label">{{ label }}</VLabel>
+  </div>
 </template>
 
 <script lang="ts" setup>
 import VLabel from "@/components/VLabel/index";
 import { useVModel } from "@vueuse/core";
-import { computed, onMounted, ref } from "vue";
+import { computed } from "vue";
 
 const props = defineProps<{
-	id: string;
-	modelValue?: any;
-	customClass?: string;
-	autoFocus?: boolean;
-	resettable?: boolean;
-	label?: string;
+  id: string;
+  modelValue?: any;
+  customClass?: string;
+  autoFocus?: boolean;
+  resettable?: boolean;
+  label?: string;
 }>();
 
 const emit = defineEmits(["update:modelValue", "onReset"]);
 
 const model = useVModel(
-	props,
-	"modelValue",
-	emit,
-	props.modelValue
-		? {}
-		: {
-				passive: true,
-				defaultValue: null,
-		  }
+  props,
+  "modelValue",
+  emit,
+  props.modelValue
+    ? {}
+    : {
+        passive: true,
+        defaultValue: null,
+      }
 );
 
-const input = ref<HTMLElement | null>(null);
-
 function toggleValue() {
-	if (props.resettable) {
-		if (model.value === false) {
-			model.value = null;
-			emit("onReset");
-		} else {
-			model.value = !model.value;
-		}
-	} else {
-		model.value = !model.value;
-	}
+  console.log("checkbox clicked");
+
+  if (props.resettable) {
+    if (model.value === false) {
+      model.value = null;
+      emit("onReset");
+    } else {
+      model.value = !model.value;
+    }
+  } else {
+    model.value = !model.value;
+  }
 }
 
 const checkboxClass = computed(() => {
-	if (model.value === true) {
-		return "true";
-	}
+  if (model.value === true) {
+    return "true";
+  }
 
-	if (model.value === false && props.resettable) {
-		return "resettable-false text-color-text";
-	}
-	return "text-color-text";
+  if (model.value === false && props.resettable) {
+    return "resettable-false text-color-text";
+  }
+  return "text-color-text";
 });
 
 const checkboxIcon = computed(() => {
-	if (model.value === true) {
-		return "fa-check";
-	}
+  if (model.value === true) {
+    return "fa-check";
+  }
 
-	if (model.value === false && props.resettable) {
-		return "fa-solid fa-xmark";
-	}
-	return undefined;
-});
-
-onMounted(() => {
-	if (props.autoFocus && input.value) {
-		input.value.focus();
-	}
+  if (model.value === false && props.resettable) {
+    return "fa-solid fa-xmark";
+  }
+  return undefined;
 });
 </script>
--- src/components/VDataGrid/VDataEditor.vue
+++ src/components/VDataGrid/VDataEditor.vue
@@ -1,46 +1,45 @@
 <template>
-	<VCheckBox
-		v-model="model"
-		v-if="isBoolean || isEditableBoolean"
-		class="w-full h-full"
-		:id="id"
-		:auto-focus="autoFocus"
-		resettable
-	/>
-	<VDatePicker
-		v-model="model"
-		v-else-if="isDate"
-		:id="id"
-		class="w-full flex items-center h-full"
-		:auto-focus="autoFocus"
-	/>
-	<VSelect
-		:id="id"
-		v-model="model"
-		:values="values!"
-		:formatter="formatter"
-		append-to="body"
-		class="w-full"
-		:auto-focus="autoFocus"
-		v-else-if="isSelect"
-	>
-	</VSelect>
-	<VNumericField
-		:id="id"
-		v-model="model"
-		v-else-if="isNumber"
-		:auto-focus="autoFocus"
-		:placeholder="placeholder"
-		class="w-full"
-	/>
-	<VTextField
-		v-model="model"
-		v-else
-		:id="id"
-		:auto-focus="autoFocus"
-		:placeholder="placeholder"
-		class="w-full"
-	/>
+  <VCheckBox
+    v-model="model"
+    v-if="isBoolean || isEditableBoolean"
+    class="w-full h-full"
+    :id="id"
+    resettable
+  />
+  <VDatePicker
+    v-model="model"
+    v-else-if="isDate"
+    :id="id"
+    class="w-full flex items-center h-full"
+    :auto-focus="autoFocus"
+  />
+  <VSelect
+    :id="id"
+    v-model="model"
+    :values="values!"
+    :formatter="formatter"
+    append-to="body"
+    class="w-full"
+    :auto-focus="autoFocus"
+    v-else-if="isSelect"
+  >
+  </VSelect>
+  <VNumericField
+    :id="id"
+    v-model="model"
+    v-else-if="isNumber"
+    :auto-focus="autoFocus"
+    :placeholder="placeholder"
+    class="w-full"
+  />
+  <VTextField
+    v-model="model"
+    v-else
+    :id="id"
+    :auto-focus="autoFocus"
+    :placeholder="placeholder"
+    class="w-full"
+  />
 </template>
 
 <script setup lang="ts">
@@ -53,19 +52,19 @@ import { useVModel } from "@vueuse/core";
 import VNumericField from "../VNumericField/index";
 
 const props = withDefaults(
-	defineProps<{
-		id: string;
-		type?: VDataType;
-		values?: any[];
-		modelValue: any;
-		formatter?: Function;
-		autoFocus?: boolean;
-		placeholder?: string;
-	}>(),
-	{
-		autoFocus: false,
-		formatter: (value: any) => value,
-	}
+  defineProps<{
+    id: string;
+    type?: VDataType;
+    values?: any[];
+    modelValue: any;
+    formatter?: Function;
+    autoFocus?: boolean;
+    placeholder?: string;
+  }>(),
+  {
+    autoFocus: false,
+    formatter: (value: any) => value,
+  }
 );
 
 const emit = defineEmits(["update:modelValue"]);
@@ -73,14 +72,14 @@ const emit = defineEmits(["update:modelValue"]);
 const model = useVModel(props, "modelValue", emit);
 
 const isBoolean =
-	props.type !== undefined && (props.type as VDataType) === VDataType.BOOLEAN;
+  props.type !== undefined && (props.type as VDataType) === VDataType.BOOLEAN;
 const isEditableBoolean =
-	props.type !== undefined &&
-	(props.type as VDataType) === VDataType.EDITABLE_BOOLEAN;
+  props.type !== undefined &&
+  (props.type as VDataType) === VDataType.EDITABLE_BOOLEAN;
 const isDate =
-	props.type !== undefined && (props.type as VDataType) === VDataType.DATE;
+  props.type !== undefined && (props.type as VDataType) === VDataType.DATE;
 const isSelect =
-	props.type !== undefined && (props.type as VDataType) === VDataType.SELECT;
+  props.type !== undefined && (props.type as VDataType) === VDataType.SELECT;
 const isNumber =
-	props.type !== undefined && (props.type as VDataType) === VDataType.NUMBER;
+  props.type !== undefined && (props.type as VDataType) === VDataType.NUMBER;
 </script>
